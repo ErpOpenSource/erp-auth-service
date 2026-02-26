65e3068efcf162b7984c55e6b53cf113986cbb89
Juan Frauca <juancore97@gmail.com>
2026-02-26 15:54:29 +0100

    build(bootstrap): init Spring Boot skeleton

---
+++ src/main/java/com/erp/auth/ErpAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erp.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpAuthServiceApplication.class, args);
+	}
+
+}
